fb15f2a430895d7e36b252896c62c8eec4ca1aad
DOMINGUESJULIAM <dominguesjuliam@gmail.com>
2024-05-14 10:34:01 -0300

    Criação da classe cep

---
+++ lib/cep.dart
@@ -0,0 +1,17 @@
+class Cep {
+  String cep;
+  String logradouro;
+  String complemento;
+  String bairro;
+  String localidade;
+  String uf;
+
+  Cep({
+    required this.cep,
+    required this.logradouro,
+    required this.complemento,
+    required this.bairro,
+    required this.localidade,
+    required this.uf,
+  });
+}
